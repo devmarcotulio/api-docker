6e43d53a0b9ec677b3e77c25d3cc711bd87ed3f3
Marco Túlio <marcotulio_iub@hotmail.com>
2024-09-22 22:04:46 -0300

    feat: docker start controller

---
+++ src/controllers/DockerStartController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+import DockerStartService from '../services/DockerStartService';
+
+class DockerStartController {
+  constructor(private dockerStartService: DockerStartService) { }
+
+  async handle(req: Request, res: Response): Promise<void> {
+    const { containerId } = req.body;
+    await this.dockerStartService.execute(containerId)
+      .then(() => res.status(200).json({ message: `Container ${containerId} iniciado com sucesso!`, success: true }))
+      .catch((err) => res.status(400).json({ message: `Falha ao iniciar o container ${containerId} - ${err}`, success: false }))
+  }
+}
+
+export default DockerStartController;
