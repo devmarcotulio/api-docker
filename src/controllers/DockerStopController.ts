5a8fa3f57e29c6e10858019e52c69543460b300b
Marco Túlio <marcotulio_iub@hotmail.com>
2024-09-22 22:05:07 -0300

    feat: docker stop controller

---
+++ src/controllers/DockerStopController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+import DockerStopService from "../services/DockerStopService";
+
+class DockerStopController {
+  constructor(private dockerStopService: DockerStopService) { }
+
+  async handle(req: Request, res: Response): Promise<void> {
+    const { containerId } = req.body;
+    await this.dockerStopService.execute(containerId)
+      .then(() => res.status(200).json({ message: `Container ${containerId} parado com sucesso!`, success: true }))
+      .catch((err) => res.status(400).json({ message: `Falha ao parar o container ${containerId} - ${err}`, success: false }))
+  }
+}
+
+export default DockerStopController;
