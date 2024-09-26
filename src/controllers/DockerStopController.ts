5743dfabe61bb1b3eb4d01fc357eed1f2bef69db
Marco Túlio <marcotulio_iub@hotmail.com>
2024-09-26 20:45:07 -0300

    fix: docker stop

--- src/controllers/DockerStopController.ts
+++ src/controllers/DockerStopController.ts
@@ -4,13 +4,13 @@ import DockerStopService from "../services/DockerStopService";
 class DockerStopController {
   constructor(private dockerStopService: DockerStopService) { }
 
-  async handle(req: Request, res: Response): Promise<void> {
+  async handle(req: Request, res: Response): Promise<Response> {
     const { containerId } = req.body;
     try {
-      await this.dockerStopService.execute(containerId);
-      res.status(200).json({ message: `Container ${containerId} parado com sucesso!`, success: true });
+      const result = await this.dockerStopService.execute(containerId);
+      return res.status(result.success ? 200 : 409).json(result);
     } catch (err) {
-      res.status(400).json({ message: `Falha ao parar o container ${containerId} - ${err}`, success: false });
+      return res.status(400).json({ message: `Falha ao parar o container ${containerId} - ${err}`, success: false });
     }
   }
 }
--- src/services/DockerStopService.ts
+++ src/services/DockerStopService.ts
@@ -2,7 +2,19 @@ import docker from "../config/DockerConfig";
 
 class DockerStopService {
   async execute(containerId: string) {
-    await docker.getContainer(containerId).stop();
+    const container = docker.getContainer(containerId);
+    const infoContainer = await container.inspect();
+    if (!infoContainer.State.Running) {
+      return {
+        message: `O container ${containerId} não está em execução.`,
+        success: false
+      }
+    }
+    await container.stop();
+    return {
+      message: `Container ${containerId} parado com sucesso.`,
+      success: true
+    }
   }
 }
 
