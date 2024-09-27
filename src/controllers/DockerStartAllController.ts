698cfd1fdcef43cc2a9cb086dda8caa9cbb92e8e
Marco Túlio <marcotulio_iub@hotmail.com>
2024-09-26 21:08:53 -0300

    fix: docker start all

--- src/controllers/DockerStartAllController.ts
+++ src/controllers/DockerStartAllController.ts
@@ -7,7 +7,7 @@ class DockerStartAllController {
   async handle(req: Request, res: Response): Promise<Response> {
     try {
       const containers = await this.dockerStartAllService.execute();
-      return res.status(200).json({ message: `Containers iniciados: ${containers.started.length} - Containers que já estavam em execução: ${containers.alreadyStarted.length}`, success: true });
+      return res.status(containers.started.length > 0 ? 200 : 409).json({ message: `Container(s) iniciado(s): ${containers.started.length} - Container(s) que já estava(m) em execução: ${containers.alreadyStarted.length}`, success: true });
     } catch (err) {
       return res.status(400).json({ message: `Falha ao iniciar todos os containers - ${err}`, success: false });
     }
